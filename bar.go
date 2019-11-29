eea58bd256827148e3b237e3d00ca1054f59254a
k.sumi <dot.deamon@gmail.com>
2019-11-29 19:39:16 +0900

    #32 fix

--- bar.go
+++ bar.go
@@ -112,8 +112,7 @@ func (b *Bar) Incr() bool {
 	if n > b.Total {
 		return false
 	}
-	var t time.Time
-	if b.TimeStarted == t {
+	if b.TimeStarted.IsZero() {
 		b.TimeStarted = time.Now()
 	}
 	b.timeElapsed = time.Since(b.TimeStarted)
